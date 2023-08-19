437b527b910375893911e2714654b3e5aec39bdf
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-08-19 04:44:29 +0300

    MessageBroker ile MicroServis Mimari ile haberleşmesi eklendi.

---
+++ Service5/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://bnalwiyy:eraoNBcELGIxbK2mFVLlUJeGK1p9LMHi@shark.rmq.cloudamqp.com/bnalwiyy");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example",
+        body: byteMessage
+
+        );
+}
+
+Console.Read();
